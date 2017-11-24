47eadf94586bfd167b7e5fbf84e54f0cb5e1023a
KarimChukfeh <kchuk030@uottawa.ca>
2017-11-24 18:00:46 -0500

    pi works

--- ruby/Pi.rb
+++ ruby/Pi.rb
@@ -1,5 +1,5 @@
 def inCircle(x,y)
-   if (((x-1).abs ** 2) + ((y-1).abs ** 2)) < (1 ** 2)
+   if (((x-1).abs ** 2) + ((y-1).abs ** 2))**2 < (1**2)
      true
    else
      false
@@ -10,11 +10,11 @@ total = 100000
 incircle = 0
 
 for i in 0..total
-  x = rand(0..400)/200
-  y = rand(0..400)/200
+  x = rand(0.0 .. 2.0)
+  y = rand(0.0 .. 2.0)
   if inCircle(x,y)
     incircle += 1
   end
 end
 
-puts 4*(incircle/total)
+puts 4*(incircle.to_f/total.to_f)
